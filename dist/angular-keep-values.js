9089d09d8693f1c8fc664ce57ae57a05dd0b744a
René A. Morales <rene.morales.sanchez@gmail.com>
2014-12-12 17:58:08 -0300

    chore(bower): release 0.1.1

--- dist/angular-keep-values.js
+++ dist/angular-keep-values.js
@@ -1,6 +1,6 @@
 /**
  * Keep your input values in your ngModels
- * @version v0.1.0 - 2014-12-12
+ * @version v0.1.1 - 2014-12-12
  * @link https://github.com/platanus/angular-keep-values
  * @author Emilio Blanco <emilioeduardob@gmail.com>, Jaime Bunzli <jpbunzli@gmail.com>, René Morales <rene.morales.sanchez@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -9,4 +9,41 @@
 (function(angular, undefined) {
 'use strict';
 angular
-  .module('platanus.keepValues', []);})(angular);
+  .module('platanus.keepValues', []);
+angular
+  .module('platanus.keepValues')
+  .directive('keepCurrentValue', keepCurrentValue);
+
+function keepCurrentValue() {
+  var directive = {
+    link: link,
+    restrict: 'A',
+    require: 'ngModel'
+  };
+
+  return directive;
+
+  function link(scope, element, attrs, controller) {
+    controller.$setViewValue(attrs.value);
+  }
+}
+angular
+  .module('platanus.keepValues')
+  .directive('keepInputValues', keepInputValues);
+
+function keepInputValues() {
+  var directive = {
+    link: link,
+    restrict: 'A'
+  };
+
+  return directive;
+
+  function link(scope, element, attrs) {
+    var inputs = element.find('input');
+    angular.forEach(inputs, function(input){
+      var x = angular.element(input);
+      x.attr('keep-current-value', '');
+    });
+  }
+}})(angular);
