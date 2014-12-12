aa8390b111438439883e3b17182cddb1fa28e08d
René A. Morales <rene.morales.sanchez@gmail.com>
2014-12-12 17:35:38 -0300

    release(0.1.0): grunt build

---
+++ dist/angular-keep-values.js
@@ -0,0 +1,12 @@
+/**
+ * Keep your input values in your ngModels
+ * @version v0.1.0 - 2014-12-12
+ * @link https://github.com/platanus/angular-keep-values
+ * @author Emilio Blanco <emilioeduardob@gmail.com>, Jaime Bunzli <jpbunzli@gmail.com>, René Morales <rene.morales.sanchez@gmail.com>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+(function(angular, undefined) {
+'use strict';
+angular
+  .module('platanus.keepValues', []);})(angular);
