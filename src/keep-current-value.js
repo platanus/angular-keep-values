536edc9d7eb8ee88b838549425d73f5c7d4be4f8
René A. Morales <rene.morales.sanchez@gmail.com>
2014-12-19 12:09:51 -0300

    feat(): adds support for <select> and <textarea> tags

--- src/keep-current-value.js
+++ src/keep-current-value.js
@@ -1,3 +1,20 @@
+var getViewValueFrom = {
+  SELECT: function(element) {
+    var options = element.find('option');
+    for (var i = 0; i < options.length; i++) {
+      var el = angular.element(options[i].outerHTML);
+      if ( el.attr('selected') ) return el.attr('value');
+    };
+    if ( options[0] ) return angular.element(options[0]).attr('value');
+  },
+  INPUT: function(element) {
+    return element.attr('value');
+  },
+  TEXTAREA: function(element) {
+    return element.html();
+  }
+}
+
 angular
   .module('platanus.keepValues')
   .directive('keepCurrentValue', keepCurrentValue);
@@ -12,6 +29,19 @@ function keepCurrentValue() {
   return directive;
 
   function link(scope, element, attrs, controller) {
-    controller.$setViewValue(attrs.value);
+    var viewValue = getViewValueFromElement(element);
+    if ( viewValue ) {
+      controller.$setViewValue(viewValue);
+      controller.$render();
+    }
   }
 }
+
+function getViewValueFromElement(element) {
+  var viewValue;
+  var tagName = element[0].tagName;
+
+  if ( SUPPORTED_ELEMENTS.indexOf(tagName) > -1 ) return getViewValueFrom[tagName](element);
+  return false;
+}
+
